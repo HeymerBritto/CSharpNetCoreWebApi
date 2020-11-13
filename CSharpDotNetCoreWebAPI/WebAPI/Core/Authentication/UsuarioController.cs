6444999c4f94daa9793ead27123931afcd2c4d10
Heymer Carmona de Britto <heymer_A7X@live.com>
2020-11-12 21:27:06 -0300

    Projeto Sem Identity

---
+++ CSharpDotNetCoreWebAPI/WebAPI/Core/Authentication/UsuarioController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models.Authentication;
+using System;
+using WebAPI.Core.JWT;
+
+namespace WebAPI.Core.Authentication
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public ActionResult<dynamic> Authenticate([FromBody] Usuario model)
+        {
+            // Recupera o usuário
+            var user = UsuarioRepository.Get(model.Username, model.Password);
+
+            // Verifica se o usuário existe
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            // Gera o Token
+            var token = JWTService.GenerateToken(user);
+
+            // Oculta a senha
+            user.Password = "";
+
+            // Retorna os dados
+            return new
+            {
+                user,
+                token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
---
+++ CSharpDotNetCoreWebAPI/WebAPI/Core/Authentication/UsuarioRepository.cs
@@ -0,0 +1,20 @@
+﻿using Models.Authentication;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WebAPI.Core.Authentication
+{
+    public class UsuarioRepository
+    {
+        public static Usuario Get(string username, string password)
+        {
+            var users = new List<Usuario>
+            {
+                new Usuario { Id = 1, Username = "batman", Password = "batman", Role = "manager" },
+                new Usuario { Id = 2, Username = "robin", Password = "robin", Role = "employee" }
+            };
+
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
